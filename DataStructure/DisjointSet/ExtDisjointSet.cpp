a901a3cb43894895b2a7db8f9645a749b9f88187
Zejun Wu <zejun.wu@gmail.com>
2013-03-13 20:10:50 +0800

    Maintain set size in ExtDisjointSet

--- DataStructure/DisjointSet/ExtDisjointSet.cpp
+++ DataStructure/DisjointSet/ExtDisjointSet.cpp
@@ -1,38 +1,48 @@
 struct DisjointSet {
-    int p[MAXN];
+  int p[MAXN], s[MAXN], t[MAXN];
 
-    static inline int RE(int i) { return -1 - i; }
+  static inline int RE(int i) { return -1 - i; }
 
-    void init(int n) {
-        for (int i = 0; i < n; ++i) {
-            p[i] = i;
-        }
+  void init(int n) {
+    for (int i = 0; i < n; ++i) {
+      p[i] = i;
+      s[i] = 1;
+      t[i] = 0;
     }
+  }
 
-    int getp(int i) {
-        if (i < 0) {
-            return RE(getp(RE(i)));
-        } else if (i == p[i]) {
-            return i;
-        } else {
-            return p[i] = getp(p[i]);
-        }
+  int getp(int i) {
+    if (i < 0) {
+      return RE(getp(RE(i)));
+    } else if (i == p[i]) {
+      return i;
+    } else {
+      return p[i] = getp(p[i]);
     }
+  }
 
-    int setp(int i, int j) {
-        i = getp(i);
-        j = getp(j);
-        if (i == j) {
-            return 0;
-        } else if (i == RE(j)) {
-            return -1;
-        } else {
-            if (i < 0) {
-                i = RE(i);
-                j = RE(j);
-            }
-            p[i] = j;
-            return 1;
-        }
+  int setp(int i, int j) {
+    i = getp(i);
+    j = getp(j);
+    if (i == j) {
+      return 0;
+    } else if (i == RE(j)) {
+      return -1;
+    } else {
+      if (i < 0) {
+        i = RE(i);
+        j = RE(j);
+      }
+      p[i] = j;
+      if (j >= 0) {
+        s[j] += s[i];
+        t[j] += t[i];
+      } else {
+        s[RE(j)] += t[i];
+        t[RE(j)] += s[i];
+      }
+      return 1;
     }
+  }
 } ds;
+
